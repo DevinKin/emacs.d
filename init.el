67b4d2582383590dc8b8178a55ea77e98208805a
devinkin <devinkin@163.com>
2019-05-05 00:42:28 +0800

    fixed font size

--- init.el
+++ init.el
@@ -1,16 +1,20 @@
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
 
 ;; Added by Package.el.  This must come before configurations of
 ;; installed packages.  Don't delete this line.  If you don't want it,
 ;; just comment it out by adding a semicolon to the start of the line.
 ;; You may delete these explanatory comments.
-;; 初始化包
 (package-initialize)
+
 (add-to-list 'load-path "~/.emacs.d/lisp")
-(require 'init-packages)
 
 (setq custom-file (expand-file-name "custom.el" user-emacs-directory))
-(load (expand-file-name "custom.el" user-emacs-directory))
 
+(require 'init-packages)
+(require 'init-youdao)
 (require 'init-utils)
 (require 'init-org)
 (require 'init-company)
@@ -21,4 +25,9 @@
 (require 'init-ivy)
 (require 'init-dired)
 (require 'init-common-lisp)
+
+
+(when (file-exists-p custom-file)
+  (load custom-file))
+
 
---
+++ lisp/init-youdao.el
@@ -0,0 +1,6 @@
+;; 有道词典
+;; Enable Cache
+(setq url-automatic-cachin t)
+(global-set-key (kbd "C-c y") 'youdao-dictionary-search-at-point+)
+
+(provide 'init-youdao)
