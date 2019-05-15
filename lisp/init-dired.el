0cf6a4a4b648af78d7b6634779b999bb9aeb6ebb
devinkin <devinkin@163.com>
2019-05-15 21:56:36 +0800

    fixed dired-mode

--- lisp/init-dired.el
+++ lisp/init-dired.el
@@ -5,6 +5,9 @@
 
 (require 'dired-x)
 
+(put 'dired-find-alternate-file 'disabled nil)
+
+
 ;; 延迟加载
 (with-eval-after-load 'dired
   (define-key dired-mode-map (kbd "RET") 'dired-find-alternate-file))
