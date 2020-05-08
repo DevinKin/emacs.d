55e7aa58cfc437fecb266e7992c18c7d70146d61
devinkin <devinkin@163.com>
2020-05-08 17:26:23 +0800

    zenburn-theme

--- lisp/init-theme.el
+++ lisp/init-theme.el
@@ -2,8 +2,12 @@
 
 ;(load-theme 'solarized-dark t)
 
-(maybe-require-package 'atom-one-dark-theme)
-
-(load-theme 'atom-one-dark t)
+;(maybe-require-package 'atom-one-dark-theme)
+;(load-theme 'atom-one-dark t)
+(use-package zenburn-theme
+  :ensure t
+  :config
+  (load-theme 'zenburn t)
+  )
 
 (provide 'init-theme)
